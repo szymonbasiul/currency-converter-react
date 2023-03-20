dc40b86b80dee3c187ba9c97157813712a532d8c
Szymon Basiul <szymon.basiul@wp.pl>
2023-03-20 21:54:16 +0100

    main container and modules added, styled up

---
+++ src/Container/index.js
@@ -0,0 +1,10 @@
+import "./style.css";
+import Form from "../Form";
+
+const Container = () => (
+  <div className="container">
+    <Form />
+  </div>
+);
+
+export default Container;
---
+++ src/Form/index.js
@@ -0,0 +1,45 @@
+import "./style.css";
+
+const Form = () => (
+  <form className="form js-form">
+    <fieldset className="form__fieldset">
+      <legend className="form__legend"> Kalkulator walutowy </legend>
+      <p>Wybierz walutę*</p>
+      <select className="js-currencyInput">
+        <option value="EUR">EUR</option>
+        <option value="GBP">GBP</option>
+        <option value="USD">USD</option>
+      </select>
+      <p>Wybierz walutę oraz kwotę transakcji*</p>
+      <label>
+        <span className="form__labelText">Kwota:</span>
+      </label>
+      <label>
+        <input
+          className="form__field js-currencyAmount"
+          id="amount"
+          name="exchange"
+          type="number"
+          placeholder="Wprowadź kwotę do wymiany"
+          autofocus
+          min="1"
+          step="0.01"
+          required
+        />
+      </label>
+    </fieldset>
+    <label>
+      <input
+        className="form__button js-submit"
+        type="submit"
+        value="Przelicz"
+      />
+    </label>
+    <p className="form__result">
+      {" "}
+      Przeliczona wartość: <strong className="js-result"></strong>
+    </p>
+  </form>
+);
+
+export default Form;
