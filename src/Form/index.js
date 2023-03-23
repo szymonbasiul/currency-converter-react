d954475d73f41ebbbaf601ee51fcc30de4ac4e40
Szymon Basiul <szymon.basiul@wp.pl>
2023-03-23 20:06:33 +0100

    set up currency and amount states, map over currencies array

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,42 +1,59 @@
+import { useState } from "react";
+import { currencies } from "../currencies";
 import "./style.css";
 
-const Form = () => (
-  <form className="form">
-    <fieldset className="form__fieldset">
-      <legend className="form__legend"> Kalkulator walutowy </legend>
-      <p>Wybierz walutę*</p>
-      <select className="select">
-        <option value="EUR">EUR</option>
-        <option value="GBP">GBP</option>
-        <option value="USD">USD</option>
-      </select>
-      <p>Wybierz walutę oraz kwotę transakcji*</p>
-      <label>
-        <span className="form__labelText">Kwota:</span>
-      </label>
-      <label>
-        <input
-          className="form__field"
-          id="amount"
-          name="exchange"
-          type="number"
-          placeholder="Wprowadź kwotę do wymiany"
-          autofocus
-          min="1"
-          step="0.01"
-          required
-        />
-      </label>
-    </fieldset>
+const Form = () => {
+  const [amount, setAmount] = useState(currencies[0].shortName);
+  const [currency, setCurrency] = useState("");
 
-    <label>
-      <button className="form__button" type="submit">Przelicz</button>
-    </label>
-    <p className="form__result">
-      {" "}
-      Przeliczona wartość: <strong className="result">result</strong>
-    </p>
-  </form>
-);
+  return (
+    <form className="form">
+      <fieldset className="form__fieldset">
+        <legend className="form__legend"> Kalkulator walutowy </legend>
+        <p>Wybierz walutę*</p>
+        <select
+          className="select"
+          value={currency}
+          onChange={({ target }) => setCurrency(target.value)}
+        >
+          {currencies.map((currency) => (
+            <option key={currency.shortName} value={currency.shortName}>
+              {currency.fullName}
+            </option>
+          ))}
+        </select>
+        <p>Wybierz walutę oraz kwotę transakcji*</p>
+        <label>
+          <span className="form__labelText">Kwota:</span>
+        </label>
+        <label>
+          <input
+            value={amount}
+            onChange={({ target }) => setAmount(target.value)}
+            className="form__field"
+            id="amount"
+            name="exchange"
+            type="number"
+            placeholder="Wprowadź kwotę do wymiany"
+            autofocus
+            min="1"
+            step="0.01"
+            required
+          />
+        </label>
+      </fieldset>
+
+      <label>
+        <button className="form__button" type="submit">
+          Przelicz
+        </button>
+      </label>
+      <p className="form__result">
+        {" "}
+        Przeliczona wartość: <strong className="result">result</strong>
+      </p>
+    </form>
+  );
+};
 
 export default Form;
